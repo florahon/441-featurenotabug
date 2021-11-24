971f190b77657c06a6117b0b3b7473e05acc1183
joleszcz <joleszcz@umich.edu>
2021-11-23 19:57:32 -0500

    Categories

--- fridgeview frontend/FridgeView/AddItemViewController.swift
+++ fridgeview frontend/FridgeView/AddItemViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 
+class CellClass: UITableViewCell {
+    
+}
+
 protocol AddItemVCDelegate {
-    func controller(controller: AddItemViewController, didSaveItemWithName name: String, andQuantity quantity: Int, andExpr_Date expr_date: String)
+    func controller(controller: AddItemViewController, didSaveItemWithName name: String, andQuantity quantity: Int, andExpr_Date expr_date: String, andCategory category: String)
 }
 
 class AddItemViewController: UIViewController {
@@ -14,12 +18,23 @@ class AddItemViewController: UIViewController {
     @IBOutlet var button: UIButton!
     @IBOutlet var button2: UIButton!
     
+    @IBOutlet weak var btnSelectCategory: UIButton!
+    let transparentView = UIView()
+        let tableView = UITableView()
+        
+        var selectedButton = UIButton()
+        
+        var dataSource = [String]()
+    
+    var category = ""
     
     var delegate: AddItemVCDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+                tableView.dataSource = self
+                tableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
 //        imageView.backgroundColor = .secondarySystemBackground
 //
 //        button.backgroundColor = .systemBlue
@@ -48,17 +63,70 @@ class AddItemViewController: UIViewController {
     }
 
     @IBAction func save(sender: UIBarButtonItem) {
-        if let name = nameTextField.text, let quantityAsString = quantityTextField.text, let quantity = Int(quantityAsString), let expr_date = expr_dateTextField.text  {
+        if let name = nameTextField.text, let quantityAsString = quantityTextField.text, let quantity = Int(quantityAsString), let expr_date = expr_dateTextField.text {
                 // Notify Delegate
-            delegate?.controller(controller: self, didSaveItemWithName: name, andQuantity: quantity, andExpr_Date: expr_date)
+            category = selectedButton.currentTitle!
+            delegate?.controller(controller: self, didSaveItemWithName: name, andQuantity: quantity, andExpr_Date: expr_date, andCategory: category)
                 
                 // Dismiss View Controller
             dismiss(animated: true, completion: nil)
             }
         }
+    
+    func addTransparentView(frames: CGRect) {
+            let window = UIApplication.shared.keyWindow
+            transparentView.frame = window?.frame ?? self.view.frame
+            self.view.addSubview(transparentView)
+            
+            tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+            self.view.addSubview(tableView)
+            tableView.layer.cornerRadius = 5
+            
+            transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+            tableView.reloadData()
+            let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+            transparentView.addGestureRecognizer(tapgesture)
+            transparentView.alpha = 0
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+                self.transparentView.alpha = 0.5
+                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: CGFloat(self.dataSource.count * 50))
+            }, completion: nil)
+        }
+        
+        @objc func removeTransparentView() {
+            let frames = selectedButton.frame
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+                self.transparentView.alpha = 0
+                self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+            }, completion: nil)
+        }
 
+    @IBAction func onClickSelectCategory(_ sender: Any) {
+            dataSource = ["Produce", "Dairy", "Protein", "Other"]
+            selectedButton = btnSelectCategory
+            addTransparentView(frames: btnSelectCategory.frame)
+        }
 }
-
+extension AddItemViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+        removeTransparentView()
+    }
+}
 //extension AddItemViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 //
 //    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
