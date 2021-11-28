5a6e26a97b04277ecce54df980485220a151b045
joleszcz <joleszcz@umich.edu>
2021-11-28 18:01:56 -0500

    Working on recipe tab

---
+++ fridgeview frontend/FridgeView/RecipeViewController.swift
@@ -0,0 +1,90 @@
+//
+//  RecipeViewController.swift
+//  FridgeView
+//
+//  Created by Joe Oleszczak on 11/28/21.
+//
+import UIKit
+
+class RecipeViewController: UITableViewController{
+    
+    let CellIdentifier = "Cell Identifier"
+    var recipes = ["example", "list"]
+    var selected_items = [Item]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: CellIdentifier)
+        print("test")
+        for s_item in SelectItemViewController.SelectedItems.selected{
+            selected_items.append(s_item)
+            print(s_item)
+        }
+    }
+
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+        
+        // Load Items
+        loadItems()
+    }
+    
+    private func loadItems() {
+            if let filePath = pathForItems(), FileManager.default.fileExists(atPath: filePath) {
+                if var archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) as? [String] {
+                    archivedItems = archivedItems + recipes
+                }
+            }
+        }
+    
+    private func pathForItems() -> String? {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        
+        if let documents = paths.first, let documentsURL = NSURL(string: documents) {
+            return documentsURL.appendingPathComponent("items.plist")?.path
+        }
+        
+        return nil
+    }
+    
+    private func saveItems() {
+        if let filePath = pathForItems() {
+            for r in recipes{
+                NSKeyedArchiver.archiveRootObject(r, toFile: filePath)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue Reusable Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier, for: indexPath as IndexPath)
+         
+        // Fetch Item
+        let item = recipes[indexPath.row]
+         
+        // Configure Table View Cell
+        cell.textLabel?.text = item
+         
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+           return 40
+    }
+         
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 40
+    }
+
+}
